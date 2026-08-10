d4cb45e87a31a71c3d27309341858d098f9ce122
KNSN92 <kekenshin92@gmail.com>
2026-08-10 12:58:59 +0900

    update: Modにrecorderのシングルトンを追加

--- src/client/java/com/knsn92/noteblock_recorder/NoteBlockRecorderMod.java
+++ src/client/java/com/knsn92/noteblock_recorder/NoteBlockRecorderMod.java
@@ -1,10 +1,21 @@
 package com.knsn92.noteblock_recorder;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class NoteBlockRecorderMod implements ClientModInitializer {
 
+    private static NoteBlockRecorderMod instance;
+
+    public static NoteBlockRecorderMod getInstance() {
+        return instance;
+    }
+
+    public final NoteBlockRecorder recorder = new NoteBlockRecorder();
+
     @Override
     public void onInitializeClient() {
+        instance = this;
+        ClientTickEvents.START_CLIENT_TICK.register(_ -> recorder.forwardTick());
     }
 }
