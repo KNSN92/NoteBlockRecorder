83ec2d2d56409c02f517dfb18cda2cf0f732f3c4
KNSN92 <kekenshin92@gmail.com>
2026-08-10 12:59:42 +0900

    add: Command

---
+++ src/client/java/com/knsn92/noteblock_recorder/Command.java
@@ -0,0 +1,94 @@
+package com.knsn92.noteblock_recorder;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.arguments.IdentifierArgument;
+import net.minecraft.commands.synchronization.SuggestionProviders;
+import net.minecraft.network.chat.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommands.argument;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommands.literal;
+
+public class Command {
+
+    public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandBuildContext buildContext) {
+
+        var nbsrecorder = literal("nbsrecorder");
+        nbsrecorder.then(
+                literal("start").then(argument("song_name", StringArgumentType.greedyString())
+                        .executes(Command::startRecording)
+                ));
+        nbsrecorder.then(literal("cancel").executes(Command::cancelRecording));
+        nbsrecorder.then(literal("finish").executes(Command::finishRecording));
+        var sounds = literal("sounds");
+        sounds.then(literal("add")
+                .then(argument("sound", IdentifierArgument.id())
+                        .suggests(SuggestionProviders.cast(SuggestionProviders.AVAILABLE_SOUNDS))
+                        .executes(_ -> SINGLE_SUCCESS)
+                )
+        );
+        sounds.then(literal("remove")
+                .then(argument("sound", IdentifierArgument.id())
+                        .suggests(SuggestionProviders.cast(SuggestionProviders.AVAILABLE_SOUNDS))
+                        .executes(_ -> SINGLE_SUCCESS)
+                )
+        );
+        sounds.then(literal("clear").executes(_ -> SINGLE_SUCCESS));
+        sounds.then(literal("list").executes(_ -> SINGLE_SUCCESS));
+        nbsrecorder.then(sounds);
+        dispatcher.register(nbsrecorder);
+    }
+
+    private static int startRecording(CommandContext<FabricClientCommandSource> ctx) {
+        var recorder =  NoteBlockRecorderMod.getInstance().recorder;
+        if(recorder.isRecording()) {
+            ctx.getSource().sendFeedback(Component.literal("Already recording a song! Finish the current recording before starting a new one."));
+            return SINGLE_SUCCESS;
+        }
+        String songName = ctx.getArgument("song_name", String.class);
+        recorder.startRecording(songName);
+        ctx.getSource().sendFeedback(Component.literal("Started recording song: " + songName));
+        return SINGLE_SUCCESS;
+    }
+
+    private static int cancelRecording(CommandContext<FabricClientCommandSource> ctx) {
+        var recorder = NoteBlockRecorderMod.getInstance().recorder;
+        if(!recorder.isRecording()) {
+            ctx.getSource().sendFeedback(Component.literal("Not currently recording a song! Start a recording before trying to cancel one."));
+            return SINGLE_SUCCESS;
+        }
+        recorder.cancelRecording();
+        ctx.getSource().sendFeedback(Component.literal("Cancelled current recording!"));
+        return SINGLE_SUCCESS;
+    }
+
+    private static int finishRecording(CommandContext<FabricClientCommandSource> ctx) {
+        var recorder = NoteBlockRecorderMod.getInstance().recorder;
+        if(!recorder.isRecording()) {
+            ctx.getSource().sendFeedback(Component.literal("Not currently recording a song! Start a recording before trying to finish one."));
+            return SINGLE_SUCCESS;
+        }
+        String songName = recorder.getSongName();
+        String songFileName = songName.endsWith(".nbs") ? songName : songName + ".nbs";
+        byte[] nbsData = recorder.finishRecording();
+        Path saveDir = FabricLoader.getInstance().getGameDir().resolve("recorded_nbs_songs");
+        try {
+            Files.createDirectories(saveDir);
+            Path outputPath = saveDir.resolve(songFileName);
+            Files.write(outputPath, nbsData);
+            ctx.getSource().sendFeedback(Component.literal("Finished recording song '" + songName + "'. Saving to the game directory"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return SINGLE_SUCCESS;
+    }
+}
--- src/client/java/com/knsn92/noteblock_recorder/NoteBlockRecorderMod.java
+++ src/client/java/com/knsn92/noteblock_recorder/NoteBlockRecorderMod.java
@@ -1,6 +1,7 @@
 package com.knsn92.noteblock_recorder;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class NoteBlockRecorderMod implements ClientModInitializer {
@@ -17,5 +18,6 @@ public class NoteBlockRecorderMod implements ClientModInitializer {
     public void onInitializeClient() {
         instance = this;
         ClientTickEvents.START_CLIENT_TICK.register(_ -> recorder.forwardTick());
+        ClientCommandRegistrationCallback.EVENT.register(Command::register);
     }
 }
