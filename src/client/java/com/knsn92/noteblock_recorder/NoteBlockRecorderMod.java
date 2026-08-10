6f8ac1bd1499135ce35dfac4c85eb6d221dea178
KNSN92 <kekenshin92@gmail.com>
2026-08-10 12:35:24 +0900

    chore: 一旦初期設定

---
+++ src/client/java/com/knsn92/noteblock_recorder/NoteBlockRecorderMod.java
@@ -0,0 +1,10 @@
+package com.knsn92.noteblock_recorder;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoteBlockRecorderMod implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
