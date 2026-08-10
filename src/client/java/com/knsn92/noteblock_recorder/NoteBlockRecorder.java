baceaa58d8356418e8ec4f96ebd82350960c9cb1
KNSN92 <kekenshin92@gmail.com>
2026-08-10 18:15:08 +0900

    feat: NoteBlockRecorderにCustom Instrumentの書き出し機能を追加

--- src/client/java/com/knsn92/noteblock_recorder/NoteBlockRecorder.java
+++ src/client/java/com/knsn92/noteblock_recorder/NoteBlockRecorder.java
@@ -1,7 +1,9 @@
 package com.knsn92.noteblock_recorder;
 
 import com.google.common.collect.*;
+import net.minecraft.resources.Identifier;
 import org.jspecify.annotations.NonNull;
+import java.util.*;
 
 public class NoteBlockRecorder {
 
@@ -10,6 +12,8 @@ public class NoteBlockRecorder {
 
     private boolean isRecording = false;
     private int tick = 0;
+
+    private List<NbsWriter.CustomInstrument> customInstruments = new ArrayList<>();
 
     public NoteBlockRecorder() {}
 
@@ -21,7 +25,8 @@ public class NoteBlockRecorder {
         this.songName = songName;
         this.isRecording = true;
         this.tick = 0;
-        this.nbsWriter = new NbsWriter();
+        var customInstrumentsArray = this.customInstruments.toArray(NbsWriter.CustomInstrument[]::new);
+        this.nbsWriter = new NbsWriter(customInstrumentsArray);
     }
 
     public void forwardTick() {
@@ -31,6 +36,35 @@ public class NoteBlockRecorder {
     public void addNote(@NonNull Note note) {
         if(!isRecording) return;
         nbsWriter.addNote(note, tick);
+    }
+
+    public boolean addCustomInstrument(Identifier soundId, String name) {
+        if(Note.NOTE_SOUND_TO_ID.containsKey(soundId)) return false;
+        if(customInstruments.size() + Note.NOTE_SOUND_TO_ID.size() >= 256) return false;
+        String file = "sounds/" + soundId.getNamespace() + "/" + soundId.getPath() + ".ogg";
+        customInstruments.add(new NbsWriter.CustomInstrument(name, file, soundId));
+        return true;
+    }
+
+    public boolean hasCustomInstrument(Identifier soundId) {
+        return customInstruments.stream().anyMatch(ci -> ci.identifier().equals(soundId));
+    }
+
+    public boolean removeCustomInstrument(Identifier soundId) {
+        return customInstruments.removeIf(ci -> ci.identifier().equals(soundId));
+    }
+
+    public void clearCustomInstruments() {
+        customInstruments.clear();
+    }
+
+    public NbsWriter.CustomInstrument[] getCustomInstruments() {
+        return customInstruments.toArray(NbsWriter.CustomInstrument[]::new);
+    }
+
+    public NbsWriter.CustomInstrument[] getCurrentRecorderCustomInstruments() {
+        if(!isRecording) return null;
+        return nbsWriter.custom_instruments;
     }
 
     public String getSongName() {
